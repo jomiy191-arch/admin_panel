933cc0c6d23f537255f7539667efba6b1b487161
Sizning GitHub username <sizning_email@github.com>
2026-01-20 13:43:53 +0500

    Some changes

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -7,7 +7,6 @@ import Students from './pages/Students'
 const Layout = () => {
   return (
     <>
-      
       <div className="content flex">
         <Sidebar />
         <main className="flex-1 p-4">
